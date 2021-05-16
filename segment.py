c4177707e0658fe671bff1fd222b3636e111a7e4
Mads Awale <madsawale@gmail.com>
2021-05-16 15:13:24 +1000

    tested import

--- segment.py
+++ segment.py
@@ -2,49 +2,53 @@ import cv2 as cv
 import numpy as np
 from pathlib import Path
 
+def segment(file_path: str, target_path=''):
+    # file_path = "videos/video_1.mp4"
+    processed_name = Path(file_path).stem +'_processed'
+    if target_path == '':
+        target_path = f'{processed_name}.mp4'
+    fps = 20
+    res = (400, 240)
 
-file_path = "videos/video_1.mp4"
-processed_name = Path(file_path).stem +'_processed'
-fps = 20
-res = (400, 240)
+    fourcc = cv.VideoWriter_fourcc(*'MP4V')
+    out = cv.VideoWriter(target_path, fourcc, fps, res)
 
-cap = cv.VideoCapture(file_path)
-first_iter = True
+    cap = cv.VideoCapture(file_path)
+    first_iter = True
 
-fourcc = cv.VideoWriter_fourcc(*'MP4V')
-out = cv.VideoWriter(f'{processed_name}.mp4', fourcc, fps, res)
+    while True:
+        ret, frame = cap.read()
 
-while True:
-    ret, frame = cap.read()
+        if first_iter:
+            avg = np.float32(frame)
+            first_iter = False
 
-    if first_iter:
-        avg = np.float32(frame)
-        first_iter = False
+        cv.accumulateWeighted(frame, avg, 0)
 
-    cv.accumulateWeighted(frame, avg, 0)
+        background = cv.convertScaleAbs(avg)
 
-    background = cv.convertScaleAbs(avg)
+        mask = cv.bitwise_xor(frame, background)
 
-    mask = cv.bitwise_xor(frame, background)
+        segment = cv.absdiff(frame, background)
 
-    segment = cv.absdiff(frame, background)
+        result = cv.bitwise_and(segment, frame)
 
-    result = cv.bitwise_and(segment, frame)
+        # Write result frame to output video
+        out.write(result)
 
-    # Write result frame to output video
-    out.write(result)
+        cv.imshow('input', frame)
+        cv.imshow('result', result)
 
-    cv.imshow('input', frame)
-    cv.imshow('result', result)
+        #Check for keypress every frame_delay duration
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            break
+        if key == ord('p'):
+            cv.waitKey(-1) #wait until any key is pressed
 
-    #Check for keypress every frame_delay duration
-    key = cv.waitKey(1)
-    if key == ord('q'):
-        break
-    if key == ord('p'):
-        cv.waitKey(-1) #wait until any key is pressed
+    # When everything done, release the capture
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-out.release()
-cv.destroyAllWindows()
+if __name__ == '__main__': segment()
---
+++ test.py
@@ -0,0 +1,3 @@
+from segment import segment
+
+segment('videos/video_2.mp4')
