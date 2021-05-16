b09ba2560ce638c26dfcf758b9982739df389418
Mads Awale <madsawale@gmail.com>
2021-05-16 21:10:13 +1000

    improved play/pause/back

--- playback.py
+++ playback.py
@@ -6,30 +6,46 @@ def playback(video_file_path: str, fps: int, width: int, height: int, monochrome
     #capture frames from video source
     capture = cv.VideoCapture(video_file_path)
 
+    is_playing = True
+
     #calculate frame delay based on fps
     frame_delay = int(1000/fps)
 
+    # retrieve the total number of frames
+    frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+
     while True:
-        ret, frame = capture.read()
+        if is_playing:
+            ret, frame = capture.read()
 
-        #resize frame to width x height
-        resized_frame = cv.resize(frame, (width, height), fx=0, fy=0, interpolation=cv.INTER_CUBIC) 
+            #resize frame to width x height
+            resized_frame = cv.resize(frame, (width, height), fx=0, fy=0, interpolation=cv.INTER_CUBIC) 
 
-        #convert frame to grayscale if monochrome
-        if not monochrome:
-            cv.imshow('Output', resized_frame)
-        else:
-            new_frame = cv.cvtColor(resized_frame, cv.COLOR_BGR2GRAY)
-
-            cv.imshow('Output', new_frame)
+            #convert frame to grayscale if monochrome
+            if not monochrome:
+                cv.imshow('Output', resized_frame)
+            else:
+                new_frame = cv.cvtColor(resized_frame, cv.COLOR_BGR2GRAY)
+                cv.imshow('Output', new_frame)
 
         #Check for keypress every frame_delay duration
         key = cv.waitKey(frame_delay)
         if key == ord('q'):
             break
+        #wait until any key is pressed
         if key == ord('p'):
-            cv.waitKey(-1) #wait until any key is pressed
-    
+            is_playing = not is_playing
+
+        # check if 'b' is pressed and rewind video to the previous frame, but do not play
+        if key == ord('b') and not is_playing:
+            cur_frame_number = capture.get(cv.CAP_PROP_POS_FRAMES)
+            
+            prev_frame = cur_frame_number
+            if (cur_frame_number > 1):
+                prev_frame -= 1
+            
+            capture.set(cv.CAP_PROP_POS_FRAMES, prev_frame)
+
     capture.release()
     cv.destroyAllWindows()
 
--- segment.py
+++ segment.py
@@ -3,13 +3,17 @@ import numpy as np
 from pathlib import Path
 
 def segment(file_path: str, target_path=''):
-    # file_path = "videos/video_1.mp4"
+    # Edit name post processing
     processed_name = Path(file_path).stem +'_processed'
+
+    #default target path if not provided
     if target_path == '':
         target_path = f'{processed_name}.mp4'
+
     fps = 20
     res = (400, 240)
 
+    #define codec
     fourcc = cv.VideoWriter_fourcc(*'MP4V')
     out = cv.VideoWriter(target_path, fourcc, fps, res)
 
@@ -19,6 +23,7 @@ def segment(file_path: str, target_path=''):
     while True:
         ret, frame = cap.read()
 
+        #get the background without moving object
         if first_iter:
             avg = np.float32(frame)
             first_iter = False
@@ -27,10 +32,13 @@ def segment(file_path: str, target_path=''):
 
         background = cv.convertScaleAbs(avg)
 
+        #get object  with exclusion
         mask = cv.bitwise_xor(frame, background)
 
+        #get object by subtraccting background from original frame
         segment = cv.absdiff(frame, background)
 
+        #improve result by union of both
         result = cv.bitwise_and(segment, frame)
 
         # Write result frame to output video
